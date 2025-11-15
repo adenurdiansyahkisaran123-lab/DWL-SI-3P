89671131f561bd32b50033bda839b74219386e4d
adenurdiansyahkisaran123-lab <adenurdiansyahkisaran123@gmail.com>
2025-11-15 17:59:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+
+function home(){
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa, Kelas";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('aside').style.backgroundColor="white";
+    document.getElementById('lmpr').style.backgroundColor="white";
+}
+
+function mahasiswa () {
+    document.getElementById('selamat').innerHTML="Selamat Datang, Akmal Fadilah";
+}
+
+function kelas() {
+    document.getElementById('selamat').innerHTML="Selamat Datang, Kelas SI-3P";
+}
+
+function aktifitas(){
+    document.getElementById('aktifi').style.backgroundColor="#ed7684";
+    document.getElementById('aktifi1').style.backgroundColor="#ed7684";
+    document.getElementById('aside').style.backgroundColor="white";
+    document.getElementById('lmpr').style.backgroundColor="white";
+}
+
+function statistik(){
+    document.getElementById('aside').style.backgroundColor="#ed7684";
+    document.getElementById('lmpr').style.backgroundColor="white";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    
+}
+
+function dokumen(){
+    document.getElementById('lmpr').style.backgroundColor="#ed7684";
+    backgroundColor="white";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('aside').style.backgroundColor="white";
+}
+
+function signout(){
+    document.getElementById('selamat').innerHTML="Selamat Datang, Nama Mahasiswa Kelas";
+    document.getElementById('aktifi').style.backgroundColor="white";
+    document.getElementById('aktifi1').style.backgroundColor="white";
+    document.getElementById('aside').style.backgroundColor="white";
+    document.getElementById('lmpr').style.backgroundColor="white";
+}
